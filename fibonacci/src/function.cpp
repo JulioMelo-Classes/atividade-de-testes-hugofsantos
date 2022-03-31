0c95feed1f8b64843576b1b258f0ff27768e4e92
Unknown <hugo.santos.706@A307-21L.imd.local>
2022-03-31 18:12:47 -0300

    Implementa algoritimo de verificação de negativos

--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -3,6 +3,10 @@
 std::vector<unsigned int> fib_below_n( unsigned int n )
 {
     // TODO: adicione o seu código aqui.
+    int atual = 0;
+    int antecessor = 1;
+
+    
 
     // TODO: Isto é apenas um STUB. Troque o retorno pelo que você julgar correto.
     return std::vector<unsigned int>{};
--- negativos5/src/main.cpp
+++ negativos5/src/main.cpp
@@ -7,6 +7,15 @@ const int SIZE = 5; // input size.
 int main(void)
 {
     // TODO: Adicione aqui seu código.
+    int countNegatives = 0;
+    int number = 0;
+
+    for(int i=0;i<SIZE;i++){
+        cin >> number;
+
+        countNegatives = (number<0)?countNegatives+1:countNegatives;
+    }
+    cout << countNegatives;
 
     return 0;
 }
